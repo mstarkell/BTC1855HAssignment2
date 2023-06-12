34ae394925a72d75f0b8300fe902a49994907f7e
mstarkell <134643936+mstarkell@users.noreply.github.com>
2023-06-12 17:22:46 -0400

    #3

--- Assignment 2.R
+++ Assignment 2.R
@@ -1,15 +1,23 @@
 # Prompt user to enter a three digit positive number
 NarNum <- readline(prompt = "Please enter a three digit positive number: ")
 
+#Converts answer into numeric (rather than character)
+NarNum <- as.integer(NarNum)
+
 # Check if the answer provided to the prompt is valid. If not, print an error message
 
 if (is.numeric(as.numeric(NarNum)) & #checks if answer is numeric
     (as.numeric(NarNum)) %% 1 == 0 & #checks if answer is an integer
-    nchar(NarNum) == 3 & #checks if number is a three-digit number
-    as.numeric(NarNum) > 0) { #checks if the number is positive
-  print("This number is valid")
+    nchar(NarNum) == 3 & #checks if answer is a three-digit number
+    as.numeric(NarNum) > 0) { #checks if the answer is a positive number
+  if (((NarNum %/% 100)^3) + (((NarNum %% 100) %/% 10)^3) + ((NarNum %% 10)^3) == NarNum) { #formula for identifying an Armstrong number
+    print(paste(NarNum, "is a narcissistic number."))
+  } else {
+    print(paste(NarNum, "is not a narcissistic number."))
+  }
 } else {
   print("This entry is not valid. Terminating...")
 }
 
-NarNum <- as.integer(NarNum)
+
+
